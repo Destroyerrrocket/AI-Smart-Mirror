1b528a5f126f2ecb92b3a617f3ad397a86e23bac
destroyerrocket <polmarcetsarda@gmail.com>
2017-05-24 11:56:26 +0200

    Update streamYoutube.sh

--- streamYoutube.sh
+++ streamYoutube.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 lynx -dump "https://www.youtube.com/results?search_query=\"$1\"" | egrep -o "http.*watch.*" | vlc -
+#Now it's not used any more. It's useless. but it still can be used from the command line!
